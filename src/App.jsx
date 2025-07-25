1542195a44bd687bf57af7be81afef54cc316cbb
novisior <Sharma71845@example.com>
2025-07-25 12:16:28 +0530

    your message here

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,41 @@
-import React, { useState } from 'react'
+import React, { use, useState } from 'react'
 
 const App = () => {
-  const [number, setNumber] = useState(5)
+  const [number, setNumber] = useState(0)
+  const [array, setArray] = useState([1, 2, 3, 4, 5, 6])
 
-  // const[num , setNum]
+
+  const increment =()=>{
+    setNumber((prev)=>prev+1)
+  }
+
+  const decrement =()=>{
+    setNumber((prev)=>prev-1)
+  }
+
+  const deleteElem=()=>{
+    setArray((prev)=>prev.slice(0,-1))
+  }
   return (
     <div>
-      <h1>hello world</h1>
-      <p>
+      <h1> COUNTER!!!!!!!!!!!</h1>
+      {/* <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores,
         libero deserunt exercitationem fuga ipsam nulla laboriosam? Itaque
         accusantium expedita voluptates.
-      </p>
+        </p> */}
+        <button onClick={() => {increment()}}>+</button> 
       <h1>{number}</h1>
-      <button onClick={() => setNumber(number + 1)}>+</button>
+      {/* or can also do like this*/}
+      <button onClick={decrement}>-</button> 
+
+      <h2>{array}</h2>
+      <ul>
+        {array.map((item, index) => (
+          <li key={index}>{item}</li>
+          ))}
+      </ul>
+      <button onClick={deleteElem}>DELETE</button>
     </div>  
   )
 }
